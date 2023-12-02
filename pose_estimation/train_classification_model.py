fb869ab1bf74a158fe786265839a85e65d653705
claudioarione <claudio.arione@mail.polimi.it>
2023-12-02 10:16:42 +0100

    Update structure of classification file

--- pose_estimation/train_classification_model.py
+++ pose_estimation/train_classification_model.py
@@ -1,19 +1,32 @@
-import json
 from xgboost import XGBClassifier
 import numpy as np
 
+
+class XGBoostClassifier:
+
+    def __init__(self, np_array_path):
+        data = np.load(np_array_path)
+        self.X_train = data[:, :-1]
+        self.y_train = data[:, -1]
+
+    def train_model(self, n_estimators=100, max_depth=1, learning_rate=0.15):
+        xgbc = XGBClassifier(n_estimators, max_depth, learning_rate)
+        xgbc.fit(self.X_train, self.y_train)
+        self.classifier = xgbc
+
+    def evaluate_accuracy(self):
+        if not hasattr(self, 'classifier'):
+            print('Cannot evaluate accuracy before training the model')
+            return 0
+        return np.mean(self.y_train == self.classifier.predict(self.X_train))
+
+    def predict_class(self, X_sample):
+        return self.classifier.predict(X_sample)
+
+
 if __name__ == "__main__":
-    data = np.load("../data/training_data.npy")
-    print(data)
-    X_train = data[:, :-1]
-    y_train = data[:, -1]
-    print(X_train)
-    print(y_train)
-    xgbc = XGBClassifier(n_estimators=100, max_depth=1, learning_rate=0.15)
-    xgbc.fit(X_train, y_train)
+    classifier = XGBoostClassifier("../data/training_data.npy")
 
-    acc_test_xgbc = np.mean(y_train == xgbc.predict(X_train))
+    classifier.train_model(n_estimators=100, max_depth=1, learning_rate=0.15)
 
-    print("The XGBoost accuracy over the test sample is:", acc_test_xgbc)
-    print(X_train[:1, :])
-    print(xgbc.predict(X_train[1:2, :]))
+    print("The XGBoost accuracy over the test sample is:", classifier.evaluate_accuracy())
