f946e3a8428e76b25ae7126689f8210ff2fd24b9
Riccardo Inghilleri <riccardo.inghilleri@gmail.com>
2023-12-15 22:02:57 +0100

    Added documentation to draw_results.py

--- utils/draw_results.py
+++ utils/draw_results.py
@@ -4,13 +4,17 @@ from utils.utils import draw_text_with_background
 
 
 def _compute_line_distance(first_line, second_line):
-    # Decompose the two parameters into their coordinates
+    """
+    Computes the average distance between two lines, based on their endpoint coordinates.
+
+    :param first_line: The coordinates of the first line (x1, y1, x2, y2).
+    :param second_line: The coordinates of the second line (x1, y1, x2, y2).
+    :return: The average distance between the two lines.
+    """
     x1_l1, y1_l1, x2_l1, y2_l1 = first_line
     x1_l2, y1_l2, x2_l2, y2_l2 = second_line
-    # Check the distance among the two axis
     distance_x = (abs(x1_l2 - x1_l1) + abs(x2_l2 - x2_l1)) // 2
     distance_y = (abs(y1_l2 - y1_l1) + abs(y2_l2 - y2_l1)) // 2
-    # Assume the overall distance to be equal to the average of the twos
     return (distance_x + distance_y) // 2
 
 
@@ -22,19 +26,27 @@ def _search_posts(
     horizontal_offset,
     vertical_offset,
 ):
-    # Identify some acceptability boundaries
+    """
+    Searches for the most probable left and right goal posts in a list of lines based on the image
+    dimensions and acceptability criteria.
+
+    :param lines: A list of line coordinates detected in the image.
+    :param img_width: The width of the image.
+    :param img_height: The height of the image.
+    :param acceptability_threshold: The threshold distance for a line to be considered as a post.
+    :param horizontal_offset: The horizontal offset used to estimate the initial position of the posts.
+    :param vertical_offset: The vertical offset used to estimate the initial position of the posts.
+    :return: The estimated positions of the left and right goal posts.
+    """
     left_post_x, right_post_x = int(img_width * horizontal_offset), int(
         img_width * (1 - horizontal_offset)
     )
     post_y_low, post_y_high = int(img_height * vertical_offset), int(
         img_height * (1 - vertical_offset)
     )
-    # First estimate the position of the two posts
     estimated_left_post = [left_post_x, post_y_high, left_post_x, post_y_low]
     estimated_right_post = [right_post_x, post_y_high, right_post_x, post_y_low]
-    # Set the thresholds
     acc_thresh_left, acc_thresh_right = acceptability_threshold, acceptability_threshold
-    # Search left post in the left third of the image and right post similarly
     for line in lines:
         if _compute_line_distance(line[0], estimated_left_post) < acc_thresh_left:
             estimated_left_post = line[0]
@@ -55,28 +67,33 @@ def identify_posts(
     vertical_offset=0.3,
     draw=True,
 ):
-    # Convert the image to grayscale
+    """
+    Identifies the positions of the left and right goal posts in an image.
+
+    :param image: The input image with a football goal.
+    :param ang_coeff_threshold: The angular coefficient threshold to filter out non-vertical lines.
+    :param post_acceptability_threshold: The maximum acceptable distance for a line to be considered as a post.
+    :param horizontal_offset: The horizontal offset used to determine the search area for the posts.
+    :param vertical_offset: The vertical offset used to determine the search area for the posts.
+    :param draw: Flag indicating whether to draw the detected posts on the image.
+    :return: The positions of the left and right goal posts.
+    """
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Apply GaussianBlur to reduce noise and improve Hough Line detection
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
-    # Apply Canny edge detection
     edges = cv2.Canny(blurred, 50, 150)
 
-    # Use Hough Lines Transform to detect lines
     lines = cv2.HoughLinesP(
         edges, 1, np.pi / 180, threshold=50, minLineLength=100, maxLineGap=10
     )
 
-    # Keep only long vertical lines, which are posts candidates
     long_vertical_lines = []
     for line in lines:
         x1, y1, x2, y2 = line[0]
         if abs(x2 - x1) < ang_coeff_threshold:
             long_vertical_lines.append(line)
 
-    # Identify possible left and right post among all the candidates
     left_post, right_post = _search_posts(
         long_vertical_lines,
         image.shape[1],
@@ -85,7 +102,6 @@ def identify_posts(
         horizontal_offset,
         vertical_offset,
     )
-    # Set the height to the same value for both posts
     y_start, y_end = min(left_post[3], right_post[3]), max(left_post[1], right_post[1])
     if y_end - y_start < 340:
         if y_start < 250:
@@ -97,7 +113,6 @@ def identify_posts(
     left_post[1] = y_end
     right_post[1] = y_end
 
-    # If needed, draw posts on the original image
     if draw:
         cv2.line(
             image,
@@ -120,10 +135,18 @@ def identify_posts(
 def draw_shaded_rectangles_in_goal(
     image, left_post, right_post, percentages: list[int]
 ):
+    """
+    Draws shaded rectangles in the goal area with colors based on the given percentages.
+
+    :param image: The input image with a football goal.
+    :param left_post: The coordinates of the left goal post.
+    :param right_post: The coordinates of the right goal post.
+    :param percentages: A list of percentages to determine the color of the rectangles.
+    :return: The image with shaded rectangles drawn in the goal area.
+    """
     x1_1, y_high, x2_1, y_low = left_post
     x1_2, _, x2_2, _ = right_post
 
-    # Define boundaries
     rect_width = int(abs((x1_2 + x2_2) / 2 - (x1_1 + x2_1) / 2) / 3)
     left_rect_top_left, left_rect_bottom_right = (x1_1, y_high), (
         x1_1 + rect_width,
@@ -138,46 +161,39 @@ def draw_shaded_rectangles_in_goal(
         y_low,
     )
 
-    # Define positions for text
     text_offset_x, text_offset_y = rect_width // 3, (y_high - y_low) // 4
     left_text_start = (x1_1 + text_offset_x, y_low + text_offset_y)
     center_text_start = (x1_1 + rect_width + text_offset_x, y_low + text_offset_y)
     right_text_start = (x1_2 - rect_width + text_offset_x, y_low + text_offset_y)
 
-    # Assuming percentages is a list of three values
-    max_index = percentages.index(max(percentages))  # Index of the max percentage
-    min_index = percentages.index(min(percentages))  # Index of the min percentage
+    max_index = percentages.index(max(percentages))
+    min_index = percentages.index(min(percentages))
 
-    print(max_index, min_index)
-    print(percentages)
-
-    # Initialize default colors (for the case where max and min are the same)
     colors = [(0, 200, 200), (0, 200, 200), (0, 200, 200)]  # All yellow initially
 
     if max_index != min_index:
-        colors[max_index] = (0, 255, 0)  # Max percentage in green
-        colors[min_index] = (0, 0, 255)  # Min percentage in red
+        colors[max_index] = (0, 255, 0)
+        colors[min_index] = (0, 0, 255)
 
-    # Drawing and blending each rectangle individually
     rectangle_details = [
         (
             left_rect_top_left,
             left_rect_bottom_right,
             colors[0],
             percentages[0],
-        ),  # Blue
+        ),
         (
             center_rect_top_left,
             center_rect_bottom_right,
             colors[1],
             percentages[1],
-        ),  # Green
+        ),
         (
             right_rect_top_left,
             right_rect_bottom_right,
             colors[2],
             percentages[2],
-        ),  # Red
+        ),
     ]
 
     for top_left, bottom_right, color, percentage in rectangle_details:
@@ -185,7 +201,6 @@ def draw_shaded_rectangles_in_goal(
         cv2.rectangle(shaded_rectangle, top_left, bottom_right, color, -1)
         image = cv2.addWeighted(image, 1, shaded_rectangle, 0.5, 0)
 
-    # Add text on the image
     text_details = [
         (left_text_start, percentages[0]),
         (center_text_start, percentages[1]),
@@ -213,6 +228,13 @@ def draw_shaded_rectangles_in_goal(
 
 
 def draw_shot_predictions(image, lcr_probabilities):
+    """
+    Draws the shot prediction results on the image.
+
+    :param image: The input image with a football goal.
+    :param lcr_probabilities: A list of probabilities for left, center, and right shot directions.
+    :return: The image with shot predictions drawn.
+    """
     left_post, right_post = identify_posts(image, draw=False)
     result_image = draw_shaded_rectangles_in_goal(
         image, left_post, right_post, lcr_probabilities
@@ -229,6 +251,18 @@ def draw_dive_prediction(
     thickness_text=2,
     text_padding=12,
 ):
+    """
+    Draws the goalkeeper's dive prediction results on the image.
+
+    :param image: The input image with a football goal.
+    :param lr_probabilities: A list of probabilities for left and right dive directions.
+    :param gk_box: The bounding box of the goalkeeper.
+    :param length: The length of the arrows indicating the dive direction.
+    :param thickness_arrow: The thickness of the arrows.
+    :param thickness_text: The thickness of the text.
+    :param text_padding: The padding around the text.
+    :return: The image with dive predictions drawn.
+    """
     assert len(lr_probabilities) == 2
 
     arrow_color_left = (
